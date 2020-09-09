6442ac3255495862eead587cd9263a41d817922c
ACER <mahanasty@mail.ru>
2020-09-09 15:55:30 +0300

    добавление полос на дороге

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const score=document.querySelector(".score"),
       start=document.querySelector('.start'),
       gameArea=document.querySelector('.gameArea'),
       car=document.createElement('div');
-car.classList.add('car')
+      car.classList.add('car');
 
 
 start.addEventListener('click', startGame);
@@ -19,22 +19,47 @@ start.addEventListener('click', startGame);
        start:false,
        score:0,
        speed:3
-   }
+   };
 
 
-   function startGame() {
+function startGame() {
     start.classList.add('hide');
-    setting.start=true;
+    // добавляем линии на дороге
+    for ( let i =0;i<20;i++ ){
+       const line=document.createElement('div');
+       line.classList.add('line');
+       line.style.top=(i*100) + 'px';
+       line.y = i*100;
+       gameArea.appendChild(line)
+       }
+    setting.start = true;
+    // добавляем машинку игрока
     gameArea.appendChild(car);
+    setting.x = car.offsetLeft;
+    setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
-  }
+}
 
-  function playGame() {
-      console.log("Play Game!");
-      if(setting.start){
-          requestAnimationFrame(playGame);
-      }
-  }
+function playGame() {
+    // условия когда игра начата машинка игрока движется в направлении удерживаемых клавиш клавиш
+    if (setting.start) {
+        moveRoad();//Движение полос на дороге
+        if (keys.ArrowLeft && setting.x>0) {
+            setting.x -= setting.speed;
+        } else if (keys.ArrowRight && setting.x < (gameArea.offsetWidth-car.offsetWidth)) {
+            setting.x += setting.speed;
+        } else if (keys.ArrowDown && setting.y < (gameArea.offsetHeight-car.offsetHeight) ) {
+            setting.y += setting.speed;
+        } else if (keys.ArrowUp && setting.y >0) {
+            setting.y -= setting.speed;
+        }
+        // меняем значение  в стилях
+        car.style.left = setting.x + "px";
+        car.style.top = setting.y + "px";
+
+        requestAnimationFrame(playGame);
+    }
+}
 
   function startRun(event) {
        event.preventDefault();
@@ -44,4 +69,16 @@ start.addEventListener('click', startGame);
   function stopRun(event) {
        event.preventDefault();
        keys[event.key]=false;
+  }
+  function moveRoad(){
+    let lines=document.querySelectorAll('.line');
+    lines.forEach(function (line,i) {
+        console.log(line);
+        line.y += setting.speed;
+        line.style.top =line.y + 'px';
+        if(line.y >=document.documentElement.clientHeight){
+            line.y = -100;
+        }
+
+    })
   }
